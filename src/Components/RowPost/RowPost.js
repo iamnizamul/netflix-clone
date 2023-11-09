5fc12b64f89a14181fcc8538f5c5958372384a25
iamnizamul <itsmenizamul@gmail.com>
2023-11-10 00:57:30 +0530

    Scale on hover and Youtube

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './RowPost.css';
-import { imageUrl} from '../../Constants/Constants.js'
+import Youtube from 'react-youtube'
+import { API_KEY, imageUrl} from '../../Constants/Constants.js'
 import axios from '../../axios.js';
 
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [urlId, setUrlId] = useState('')
   useEffect(() => {
     axios.get(props.url).then((response) => {
       console.log(response.data)
@@ -12,7 +14,27 @@ function RowPost(props) {
     }).catch((err) => {
       alert('Network Error')
     })
-  })
+  }, [])
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) => {
+    console.log(id);
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
+      if (response.data.results.length!==0) {
+        setUrlId(response.data.results[0])
+      } else {
+        console.log(response.data.results.length)
+      }
+    })
+  }
+
   return (
     <div className='row'>
         <h1>{props.title}</h1>
@@ -20,9 +42,10 @@ function RowPost(props) {
           {
             movies.map((obj) =>
 
-                <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
               )}
         </div>
+        { urlId && <Youtube opts={opts} videoId={urlId.key}/> }
     </div>
   )
 }
